2865e8a5b3b4affa49d546ef3de5851ebaedcaa6
Angel Gezat <angel.gezat@etudiant.univ-brest.fr>
2024-03-04 10:39:26 +0100

    🎉 init(recuit): mise en page
    création du TP Recuit Simulé
    ⚠️n'est pas fonctionnel. le projet doit être codé⚠️

--- Recuit/Voyageur/random.c
+++ Recuit/Voyageur/random.c
@@ -1,8 +1,8 @@
 /* random.c : generation de nombres aleatoire */
 
 #include <stdio.h>
-#include <stdlib.h>     /* Pour rand, srand                 */
-#include <time.h>       /* Pour time (=> Graine pour srand) */
+#include <stdlib.h> /* Pour rand, srand                 */
+#include <time.h>	/* Pour time (=> Graine pour srand) */
 
 #include "random.h"
 
@@ -10,38 +10,42 @@
 /********* Generation de nombres aleatoires ***************************/
 /**********************************************************************/
 
-void initRandom(void)                     /* Initialisation generateur */
+void initRandom(void) /* Initialisation generateur */
 {
- static int pasEncore=1;
- if (pasEncore)
- {
-  printf("Initialisation du generateur de nombres aleatoires\n");
-  srand(time((time_t *)0)); /* Graine (generation de nombres aleatoires) */
-  pasEncore=0;
- }
+	static int pasEncore = 1;
+	if (pasEncore)
+	{
+		printf("Initialisation du generateur de nombres aleatoires\n");
+		srand(time((time_t *)0)); /* Graine (generation de nombres aleatoires) */
+		pasEncore = 0;
+	}
 }
 
-int myRandomMinMax(int min, int max)      /* Generation dans [min,max] */
+int myRandomMinMax(int min, int max) /* Generation dans [min,max] */
 {
- double interval = 0.0; int val;
+	double interval = 0.0;
+	int val;
 
- if (max<min) { int temp=min; min=max; max=temp; }
+	if (max < min)
+	{
+		int temp = min;
+		min = max;
+		max = temp;
+	}
 
- interval = (double)(max-min+1);
+	interval = (double)(max - min + 1);
 
+	val = min + (int)(interval * rand() / (RAND_MAX));
 
- val = min+(int)(interval*rand()/(RAND_MAX));
+	while (val < min || val > max)
+	{
+		val = min + (int)(interval * rand() / (RAND_MAX));
+	}
 
- while (val<min || val > max)
- {
-  val = min+(int)(interval*rand()/(RAND_MAX));
- }
-
- return val;      /* [min,max] */
+	return val; /* [min,max] */
 }
 
-double myRandom01(void)                   /* Generation dans [0.0,1.0] */
+double myRandom01(void) /* Generation dans [0.0,1.0] */
 {
- return ((double)rand()/RAND_MAX);                    /* [0.0,1.0] */
+	return ((double)rand() / RAND_MAX); /* [0.0,1.0] */
 }
-
